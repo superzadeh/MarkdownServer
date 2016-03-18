2b2d2f2431cfc7a63543deb7a06101b73be2686d
Superzadeh <charles.ahmadzadeh@outlook.com>
2016-03-18 15:31:15 +0100

    attempt to integrate instabul and coveral in travis

--- test/markdownifier.spec.js
+++ test/markdownifier.spec.js
@@ -4,7 +4,6 @@ var markdownifier = require('../src/markdownifier');
 
 describe('markdownifier.markdownify', function() {
   it('should transform markdown to html', function(done) {
-    console.log(markdownifier.markdownify('# test'));
     markdownifier.markdownify('# test').then(function(content) {
       content.should.match(/<h1 id="test">test<\/h1>/);
       done();
