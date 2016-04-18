f86495575964d48d43f958be36a8bef283e1a73f
Charles <charles.ahmadzadeh@outlook.com>
2016-04-18 17:25:21 +0200

    added tests for the credentials

--- src/credentials.js
+++ src/credentials.js
@@ -1,14 +1,11 @@
-var bluebird = require('bluebird');
 var fs = require('fs');
 
 module.exports = {
   load: () => {
-    return fs.access('./credentials.json', fs.F_OK, (err) => {
-      if (err) {
-        return { username: '', password: '' };
-      } else {
-        return credentials = JSON.parse(fs.readFileSync('./credentials.json', 'utf8'));
-      }
-    });
+    try {
+      return credentials = JSON.parse(fs.readFileSync('./credentials.json', 'utf8'));
+    } catch (error) {
+      return { username: '', password: '' };
+    }
   }
 }
